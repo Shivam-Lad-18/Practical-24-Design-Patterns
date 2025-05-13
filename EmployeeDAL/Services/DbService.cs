e6fa761a990839a4e47dd5d4434f5e32d163ffde
Shivam Lad <shivamvlad3@gmail.com>
2025-05-13 14:55:00 +0530

    Add project files.

---
+++ EmployeeDAL/Services/DbService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace EmployeeDAL.Services
+{
+    public class DbService
+    {
+        private readonly string _connectionString;
+
+        public DbService()
+        {
+            _connectionString = "Server=SF-CPU-0159\\SQLEXPRESS01;Database=DesignPatterns;Trusted_Connection=True;Encrypt=False;";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
